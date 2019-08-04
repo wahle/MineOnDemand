e1559fc4d22d1c38206dc7ddef989b36a3dcf674
wahle <wahle.4@gmail.com>
2019-08-04 12:33:11 -0400

    First pass lambda (#1)
    * do not need boto for lambda
    
    * Update to start existing stoped server
    
    * Add Game Server Start
    
    * deploy on all branches for now
    
    * Small Bugfix
    
    * Small Bugfix
    
    * Expand state and instance finding for now
    
    * Small Bugfix
    
    * Fix Debug
    
    * Fix issue with changing how boto outputs are parsed
    
    * Fix issue with changing how boto outputs are parsed
    
    * Fix issue with changing how boto outputs are parsed
    
    * add more verbose return
    
    * add more verbose return
    
    * add more verbose return
    
    * print ssh error
    
    * print ssh error
    
    * print ssh error
    
    * fix ssh error
    
    * fix ssh error
    
    * Fix ssh key type
    
    * Fix ssh key type
    
    * Fix ssh key type
    
    * Fix ssh key type
    
    * Add some ssh debug
    
    * Add some ssh debug
    
    * Add some more ssh debug

--- StartLambda/serverProvisioner.py
+++ StartLambda/serverProvisioner.py
@@ -6,36 +6,138 @@ import paramiko
 
 
 def lambda_handler(event, context):
-    #Pull From Environment Vars
-    instanceID = os.getenv("InstanceID")
-    region = os.getenv("Region")
+    statusMessage = "Wrong Password Given"
+    if event["serverPassword"] == os.getenv("MinePass"):
+        ec2 = boto3.client('ec2', region_name=os.getenv("Region"))
+        statusMessage = manageServer(ec2)
+        print(statusMessage)
+    return statusMessage
 
-    ec2 = boto3.client('ec2', region_name=region)
-    instance_id = [instanceID]
+def manageServer(client):
+    serverStatusMessage = 'ERROR unable to interact with server. Please tell someone who cares'
+    instance_id = [os.getenv("InstanceID")]
+    response = client.describe_instances(InstanceIds = instance_id)
 
-    # Start the instance
-    startResponse = ec2.start_instances(InstanceIds = instance_id)
+    reservations = response['Reservations']
+    reservation = reservations[0]
+    instances = reservation['Instances']
+    
+    print("\nSERVER INSTANCES\n")
+    print(instances)
+    print("\n")
+    if len(instances) > 0:
+        instance = instances[0]
+        state = instance['State']
+        stateName = state['Name']
 
-    # Check for successful start
+        if (stateName == 'stopped') or (stateName == 'shutting-down'):
+            #SETUP MULTIPROCESSING HERE INSTEAD OF REDIS
+            serverStartMessage = startServer(client)
+            #Redirect Route53 to new host
+            serverRoutingMessage = route53Redirect(serverStartMessage)
+            serverStatusMessage = "Server Successfully Started IP: " + serverStartMessage + serverRoutingMessage
+            javaServerStatusMessage = "ERROR Game server not started"
+            if "Successfully" in serverStatusMessage:
+                javaServerStatusMessage = startGameServer(serverStartMessage)
+            serverStatusMessage = serverStatusMessage + " " + javaServerStatusMessage
+        elif stateName == 'running':
+            serverStatusMessage = 'IP: ' + instance['PublicIpAddress']
+        else:
+            serverStatusMessage = 'ERROR ec2 instance either not found or in a bad state. Needs admin attention. ' + stateName
+    return serverStatusMessage
+
+def startServer(client):
+    #Gets proper variables to attempt to instantiate EC2 instance and start minecraft server
+    serverStatusMessage = 'ERROR, something went wrong starting ec2.'
+    instance_id = [os.getenv("InstanceID")]
+    response = client.start_instances(InstanceIds = instance_id)
+
     stateCode = 0
     while not (stateCode == 16):
         time.sleep(3)
 
         print('\nAWS EC2 START RESPONSE\n')
-        print(str(startResponse))
+        print(str(response))
         print('\n')
 
-        startResponse = ec2.describe_instances(InstanceIds = instance_id)
-        reservations = startResponse['Reservations']
+        response = client.describe_instances(InstanceIds = instance_id)
+        reservations = response['Reservations']
         reservation = reservations[0]
-
         instances = reservation['Instances']
         instance = instances[0]
-
         state = instance['State']
         stateCode = state['Code']
-        
+
         print("\nSERVER INSTANCES\n")
         print(instances)
         print("\n")
+        
+    ipAddress = instance['PublicIpAddress']
+    serverStatusMessage = ipAddress
 
+    return serverStatusMessage
+
+def route53Redirect(ipAddress):
+    serverStatusMessage = 'ERROR, something went wrong updating Route53.'
+    dnsClient = boto3.client('route53', region_name=os.getenv("Region"))
+    dnsResponse = dnsClient.change_resource_record_sets(
+        HostedZoneId=os.getenv('HostedZone'), #"Z3ULH0224N6IL2",
+        ChangeBatch={
+            "Comment": "Automatic DNS update",
+            "Changes": [
+                {
+                    "Action": "UPSERT",
+                    "ResourceRecordSet": {
+                        "Name": os.getenv('DnsName'), # "minecraft.wahl-e.name", 
+                        "Type": "A",
+                        "TTL": 180,
+                        "ResourceRecords": [
+                            {
+                                "Value": ipAddress
+                            },
+                        ],
+                    }
+                },
+            ]
+        }
+    )
+    route53Info = dnsResponse['ChangeInfo']
+    route53UpdateStatus = route53Info['Status']
+    serverStatusMessage = 'Route53 redirect status:' + route53UpdateStatus
+    return serverStatusMessage
+
+def pullFromS3(fileToCopy, bucket, localName):
+    print("File to Download {}".format(localName))
+    s3 = boto3.client('s3')
+    s3.download_file(bucket, fileToCopy, "/tmp/{}".format(localName))
+    print("/tmp/{}".format(localName))
+    return "/tmp/{}".format(localName)
+
+def startGameServer(ipAddress):
+    sshkey = pullFromS3(os.getenv('serverSshKey'), os.getenv('serverBucket'), 'serverPemKey.pem')
+    print(sshkey)
+    key = paramiko.RSAKey.from_private_key_file(sshkey)
+    sshClient = paramiko.SSHClient()
+    sshClient.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+
+    # Connect/ssh to an instance
+    try:
+        # Here 'ubuntu' is user name and 'instance_ip' is public IP of EC2
+        sshClient.connect(hostname=ipAddress, username="ubuntu", pkey=key)
+
+        # Execute a command(cmd) after connecting/ssh to an instance
+        #Vanilla Command:
+        stdin, stdout, stderr = sshClient.exec_command("screen -dmS minecraft bash -c 'sudo java -Xmx2G -jar server.jar nogui'")
+        #Modded Command: 
+        # stdin, stdout, stderr = sshClient.exec_command("screen -dmS minecraft bash -c 'sudo java -Xmx4G ${JAVA_ARGS} -jar forge-1.12.2-14.23.5.2836-universal.jar nogui'")
+        stdin.flush()
+        data = stdout.read().splitlines()
+        for line in data:
+            print(line)
+        # close the client connection once the job is done
+        sshClient.close()
+        return "Game Client Starting"
+
+    except Exception as e:
+        failMessage = "ERROR running Game server commands" + str(e)
+        return failMessage
